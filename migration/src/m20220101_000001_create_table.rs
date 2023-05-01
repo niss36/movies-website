f3975b4bf23e32b66f5a7ddbbfa9acaf902ffeae
Nissim Chekroun <nissim.chekroun@gmail.com>
2023-05-01 17:22:04 +0100

    feat: add migration to create a movies table

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -9,17 +9,20 @@ impl MigrationTrait for Migration {
         manager
             .create_table(
                 Table::create()
-                    .table(Posts::Table)
+                    .table(Movie::Table)
                     .if_not_exists()
                     .col(
-                        ColumnDef::new(Posts::Id)
+                        ColumnDef::new(Movie::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Posts::Title).string().not_null())
-                    .col(ColumnDef::new(Posts::Text).string().not_null())
+                    .col(ColumnDef::new(Movie::Title).string().not_null())
+                    .col(ColumnDef::new(Movie::ReleaseDate).date_time().not_null())
+                    .col(ColumnDef::new(Movie::PosterUrl).string().not_null())
+                    .col(ColumnDef::new(Movie::Description).text().not_null())
+                    .col(ColumnDef::new(Movie::Rating).integer().not_null())
                     .to_owned(),
             )
             .await
@@ -27,16 +30,19 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .drop_table(Table::drop().table(Posts::Table).to_owned())
+            .drop_table(Table::drop().table(Movie::Table).to_owned())
             .await
     }
 }
 
 /// Learn more at https://docs.rs/sea-query#iden
 #[derive(Iden)]
-enum Posts {
+enum Movie {
     Table,
     Id,
     Title,
-    Text,
+    ReleaseDate,
+    PosterUrl,
+    Description,
+    Rating,
 }
