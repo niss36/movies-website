6c2aa4bb5d45caff92fe12fba148112d2ca4b2e5
niss36 <nissim.chekroun@gmail.com>
2023-08-12 11:13:56 +0100

    feat!(macros): add `response(status = ...)` attribute
    BREAKING CHANGE: `#[response(status = ...)]` is now a required attribute when using the `IntoResponse` derive macro

--- macros/tests/into_response_tests.rs
+++ macros/tests/into_response_tests.rs
@@ -1,4 +1,4 @@
-use axum::response::IntoResponse;
+use axum::{http::StatusCode, response::IntoResponse};
 use macros::IntoResponse;
 
 /// Generated code:
@@ -6,38 +6,53 @@ use macros::IntoResponse;
 /// impl IntoResponse for TestEnum {
 ///   fn into_response(self) -> axum::response::Response {
 ///     match self {
-///         TestEnum::OneJsonField(body) => IntoResponse::into_response(axum::Json(body)),
-///         TestEnum::OneField(body) => IntoResponse::into_response(body),
-///         TestEnum::ZeroFields() => IntoResponse::into_response(()),
-///         TestEnum::NoFields => IntoResponse::into_response(()),
+///         TestEnum::OneJsonField(body) => IntoResponse::into_response((axum::http::StatusCode::OK, axum::Json(body))),
+///         TestEnum::OneField(body) => IntoResponse::into_response((axum::http::StatusCode::OK, body)),
+///         TestEnum::ZeroFields() => IntoResponse::into_response(axum::http::StatusCode::OK),
+///         TestEnum::NoFields => IntoResponse::into_response(axum::http::StatusCode::OK),
 ///     }
 ///   }
 /// }
 /// ```
 #[derive(IntoResponse)]
 enum TestEnum {
+    #[response(status = OK)]
     OneJsonField(#[json] Vec<String>),
+
+    #[response(status = BAD_REQUEST)]
     OneField(&'static str),
+
+    #[response(status = NOT_FOUND)]
     ZeroFields(),
+
+    #[response(status = INTERNAL_SERVER_ERROR)]
     NoFields,
 }
 
 #[test]
 fn one_json_field_into_response_works() {
-    TestEnum::OneJsonField(vec!["hello".into(), "world".into()]).into_response();
+    let response = TestEnum::OneJsonField(vec!["hello".into(), "world".into()]).into_response();
+
+    assert_eq!(response.status(), StatusCode::OK);
 }
 
 #[test]
 fn one_field_into_response_works() {
-    TestEnum::OneField("world").into_response();
+    let response = TestEnum::OneField("world").into_response();
+
+    assert_eq!(response.status(), StatusCode::BAD_REQUEST);
 }
 
 #[test]
 fn zero_fields_into_response_works() {
-    TestEnum::ZeroFields().into_response();
+    let response = TestEnum::ZeroFields().into_response();
+
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
 }
 
 #[test]
 fn no_fields_into_response_works() {
-    TestEnum::NoFields.into_response();
+    let response = TestEnum::NoFields.into_response();
+
+    assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
 }
